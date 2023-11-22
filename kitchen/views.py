18d21f1f954eb292f62821b8e2fec51faf0eb0e1
Kostia <kostia3507@gmail.com>
2023-11-22 12:42:07 +0200

    added view for main page

--- kitchen/views.py
+++ kitchen/views.py
@@ -1,3 +1,17 @@
+from django.contrib.auth import get_user_model
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from kitchen.models import Dish, DishType
+
+
+def index(request: HttpRequest) -> HttpResponse:
+    count_dishes = Dish.objects.count()
+    count_cooks = get_user_model().objects.count()
+    count_dish_types = DishType.objects.count()
+    context = {
+        "count_dishes": count_dishes,
+        "count_cooks": count_cooks,
+        "count_dish_types": count_dish_types
+    }
+    return render(request, "kitchen/index.html", context)
