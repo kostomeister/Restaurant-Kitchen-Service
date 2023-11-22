236d4dc04a065f78266fcbb2d4c13215c73dec82
Kostia <kostia3507@gmail.com>
2023-11-22 21:45:31 +0200

    added path for dish types create form template

--- kitchen/urls.py
+++ kitchen/urls.py
@@ -7,13 +7,15 @@ from kitchen.views import (
     DishListView,
     DishDetailView,
     CookListView,
-    CookDetailView
+    CookDetailView,
+    DishTypeCreateView
 )
 
 urlpatterns = [
     path("", index, name="index"),
     path("dish-types/", DishTypeListView.as_view(), name="dish-types-list"),
     path("dish-types/<int:pk>/", dish_type_detail_view, name="dish-type-detail"),
+    path("dish-types/create/", DishTypeCreateView.as_view(), name="dish-type-create"),
     path("dishes/", DishListView.as_view(), name="dishes-list"),
     path("dishes/<int:pk>/", DishDetailView.as_view(), name="dish-detail"),
     path("cooks/", CookListView.as_view(), name="cooks-list"),
