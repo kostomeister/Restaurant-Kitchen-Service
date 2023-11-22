a81519794e84b0de77d217b7264b825de1e29c4b
Kostia <kostia3507@gmail.com>
2023-11-22 11:19:59 +0200

    created model with name Dish

--- kitchen/models.py
+++ kitchen/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
@@ -10,3 +11,11 @@ class Cook(AbstractUser):
     years_of_experience = models.IntegerField()
 
 
+class Dish(models.Model):
+    name = models.CharField(max_length=64)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    dish_type = models.ForeignKey(DishType, on_delete=models.CASCADE)
+    cooks = models.ManyToManyField(
+        get_user_model(), related_name="dishes"
+    )
