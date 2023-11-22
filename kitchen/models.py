afa13c16507eb8695dfe329c5e01ca078601ce29
Kostia <kostia3507@gmail.com>
2023-11-22 11:10:09 +0200

    created model with name Cook

--- kitchen/models.py
+++ kitchen/models.py
@@ -1,7 +1,12 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
 class DishType(models.Model):
     name = models.CharField(max_length=64)
+
+
+class Cook(AbstractUser):
+    years_of_experience = models.IntegerField()
 
 
