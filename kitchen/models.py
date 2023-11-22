c5923856e038d124c0a9007005ed44c4cd83e075
Kostia <kostia3507@gmail.com>
2023-11-22 12:26:14 +0200

    added class meta to cook with verbose name

--- kitchen/models.py
+++ kitchen/models.py
@@ -10,6 +10,10 @@ class DishType(models.Model):
 class Cook(AbstractUser):
     years_of_experience = models.IntegerField(default=0)
 
+    class Meta:
+        verbose_name = "cook"
+        verbose_name_plural = "cooks"
+
 
 class Dish(models.Model):
     name = models.CharField(max_length=64)
