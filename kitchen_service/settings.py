b559727d99e4a04db747cf5e6536cf644b3dcfac
Kostia <kostia3507@gmail.com>
2023-11-24 15:12:01 +0200

    added render to allowed hosts

--- kitchen_service/settings.py
+++ kitchen_service/settings.py
@@ -33,6 +33,9 @@ DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
 ALLOWED_HOSTS = ["127.0.0.1"]
 
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Application definition
 
